5ccac63dab88922f07aac90e282001de05749dd7
Ajedrez Premium <ajedrezpremium@gmail.com>
2026-07-12 11:56:12 +0200

    fix: raw handler export without serverless-http

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
-import serverless from 'serverless-http';
 import app from '../server/src/index.js';
-export default serverless(app);
+
+export default async function handler(req, res) {
+  return app(req, res);
+}
